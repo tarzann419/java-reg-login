de65a89f8330430e6585f76db5f51030219bc8b5
dantheman <danogbo0@gmail.com>
2023-11-27 22:36:07 +0100

    initial commit

---
+++ src/main/java/com/regLogin/JavaRegLoginApplication.java
@@ -0,0 +1,13 @@
+package com.regLogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaRegLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaRegLoginApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/regLogin/JavaRegLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.regLogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaRegLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
